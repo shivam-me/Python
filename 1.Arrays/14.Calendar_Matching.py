963a9e9a6ee59f1000acc403c0271f49365a55a9
Shivam Sharan <itsshivamsharan@gmail.com>
2020-05-13 22:23:20 +0530

    Finished

--- 1.Arrays/14.Calendar_Matching.py
+++ 1.Arrays/14.Calendar_Matching.py
@@ -1,3 +1,34 @@
+'''
+ Calendar Matching
+
+ # Problem Statement
+
+Imagine that you want to schedule a meeting of a certainbduration with a co-worker. You have access to your calendar and your co-worker's
+calendar (both of which contain your respective meetings for the day, in the form of [startTime, endTime] ), as well as both of your daily
+bounds (i.e., the earliest and latest times at which you're available for meetings every day, in the form of [earliestTime, latestTime].
+
+Write a function that takes in your calendar, your daily bounds, your co-worker's calendar, your co-worker's daily bounds, and the
+duration of the meeting that you want to schedule, and that returns a list of all the time blocks (in the form of [startTime, endTime]
+during which you could schedule the meeting, ordered from earliest time block to latest.
+
+Note that times will be given and should be returned in military time. For example: 8:30 , 9:01 , and 23:56
+
+Sample Input:
+
+calendarl = [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']]
+dailybounds1 = ['9:00','20:00']
+calendar2 = [['10:007, '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']]
+dailyBounds2 = ['10:00', '18:30']
+meetingDuration = 30
+
+Sample Output :
+[['11:30', '12:00'], ['15:00', '16:00'], ['18:00', '18:30']]
+
+
+'''
+
+
+
 def calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration):
     updatedCalender1 = updateCalender(calendar1, dailyBounds1)
     updatedCalender2 = updateCalender(calendar2, dailyBounds2)
