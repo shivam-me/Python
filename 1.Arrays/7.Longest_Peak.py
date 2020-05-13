32e1902676a78dcd049a4bb084c721e16bcc3d4d
Shivam Sharan <itsshivamsharan@gmail.com>
2020-05-13 22:08:06 +0530

    Finished

--- 1.Arrays/7.Longest_Peak.py
+++ 1.Arrays/7.Longest_Peak.py
@@ -1,3 +1,29 @@
+'''
+        LONGEST PEAK
+
+PROBLEM STATEMENT:
+
+Write a function that takes array of integers and returns the length of the longest peak in the array.
+
+A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip(the highest value in the peak)
+at which point they become strictly decreasing.At least three integers are required to form a peak.
+
+For Example,the integers 1,4,10,2 form a peak,but the integers 4,0,10 don't and neither do the integers 1,2,2,0. Similarly,the
+integers 1,2,3 don't form a peak because there aren't any strictly decreasing integers after the 3.
+
+Sample Input :
+
+array = [1,2,3,4,0,10,6,5,-1,-3,2,3]
+
+Sample Output :
+
+6 # 0,10,6,5,-1,-3
+
+
+
+
+'''
+
 def longestPeak(array):
     longestPeakLen = 0
     i = 1
@@ -8,10 +34,12 @@ def longestPeak(array):
             continue
 
         leftIdx = i - 2
+
         while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
             leftIdx -= 1
 
         rightIdx = i + 2
+
         while rightIdx < len(array) and array[rightIdx - 1] > array[rightIdx]:
             rightIdx += 1
 
@@ -21,3 +49,5 @@ def longestPeak(array):
 
     return longestPeakLen
 
+
+print(longestPeak([1, 2, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
--- 1.Arrays/8.Four_Number_Sum.py
+++ 1.Arrays/8.Four_Number_Sum.py
@@ -1,4 +1,20 @@
-#   From AlgoExpert.io solution
+'''
+Four Number Sum
+
+#### Problem Statement
+
+Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should nd all quadruplets in the array
+that sum up to the target sum and return a two-dimensional array of all these quadruplets in no particular order. If no four numbers sum up to the target sum, the
+function should return an empty array.
+
+
+`Sample input: [7, 6, 4, -1, 1, 2], 16
+Sample output: [[7, 6, 4, -1], [7, 6, 1, 2]]`
+
+
+#### Solution
+
+'''
 #   Average:    O(n^2) time  | O(n^2) space
 #   Worst:      O(n^3) time  | O(n^2) space
 def fourNumberSum(array, targetSum):
